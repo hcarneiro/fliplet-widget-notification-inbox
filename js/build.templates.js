6beb9c174869ae3e425a7d68ef6d134099e7981f
OleksandrLavrynenko <86256215+OleksandrLavrynenko@users.noreply.github.com>
2022-02-07 11:49:26 +0200

    [Fix] fix date format (#37)

--- js/build.templates.js
+++ js/build.templates.js
@@ -11,7 +11,7 @@ this["Fliplet"]["Widget"]["Templates"]["templates.noNotifications"] = Handlebars
 },"useData":true});
 
 this["Fliplet"]["Widget"]["Templates"]["templates.notification"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div class=\"notification {{#if readStatus}}notification-read{{else}}notification-unread{{/if}}{{#if hasLink}} notification-linked{{/if}}\" data-notification-id=\"{{id}}\" data-notification-status=\"{{status}}\">\r\n  <div class=\"subtitle\">{{TD orderAt calendar=\"null\"}}</div>\r\n  {{#if data.title}}<h2 class=\"title\">{{data.title}}</h2>{{/if}}\r\n  {{#if data.message}}<div class=\"description\">\r\n    <p>{{data.message}}</p>\r\n  </div>{{/if}}\r\n  {{#unless readStatus}}<div class=\"notification-badge\"></div>{{/unless}}\r\n</div>";
+    return "<div class=\"notification {{#if readStatus}}notification-read{{else}}notification-unread{{/if}}{{#if hasLink}} notification-linked{{/if}}\" data-notification-id=\"{{id}}\" data-notification-status=\"{{status}}\">\r\n  <div class=\"subtitle\">{{TD orderAt format=\"fromNow\"}}</div>\r\n  {{#if data.title}}<h2 class=\"title\">{{data.title}}</h2>{{/if}}\r\n  {{#if data.message}}<div class=\"description\">\r\n    <p>{{data.message}}</p>\r\n  </div>{{/if}}\r\n  {{#unless readStatus}}<div class=\"notification-badge\"></div>{{/unless}}\r\n</div>\r\n";
 },"useData":true});
 
 this["Fliplet"]["Widget"]["Templates"]["templates.notificationsError"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
