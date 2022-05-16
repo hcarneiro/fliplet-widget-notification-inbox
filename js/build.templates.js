ffb8c38773bbfe9f31e95b6044ee8c5996d916c4
OleksandrLavrynenko <86256215+OleksandrLavrynenko@users.noreply.github.com>
2022-05-16 11:04:42 +0300

    OD-577 [Feature] Added translation to Widget-NotificationInbox (#35)
    * [ADD] add translation
    
    * [FIX] fix translation
    
    * [ADD] add translation
    
    * [FIX] fix translations
    
    * [FIX] fix translations
    
    * [FIX] fixed spacing
    
    * [ADD] add translate number
    
    * [FIX] fix translation
    
    * [FIX] fix translation
    
    * [ADD] init
    
    * [Add] added translation count of unread msg
    
    * [Fix] duplicate translation removed
    
    * [Fix] fix date format

--- js/build.templates.js
+++ js/build.templates.js
@@ -3,11 +3,19 @@ this["Fliplet"]["Widget"] = this["Fliplet"]["Widget"] || {};
 this["Fliplet"]["Widget"]["Templates"] = this["Fliplet"]["Widget"]["Templates"] || {};
 
 this["Fliplet"]["Widget"]["Templates"]["templates.loadMore"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div class=\"load-more\">\r\n  <button data-load-more class=\"btn btn-link btn-block btn-lg\">Load more</button>\r\n  <i class=\"fa fa-spinner fa-pulse fa-3x fa-fw\"></i>\r\n</div>\r\n";
+    return "<div class=\"load-more\">\r\n  <button data-load-more class=\"btn btn-link btn-block btn-lg\">"
+    + container.escapeExpression((helpers.T || (depth0 && depth0.T) || helpers.helperMissing).call(depth0 != null ? depth0 : (container.nullContext || {}),"widgets.notificationInbox.actions.loadMore",{"name":"T","hash":{},"data":data}))
+    + "</button>\r\n  <i class=\"fa fa-spinner fa-pulse fa-3x fa-fw\"></i>\r\n</div>\r\n";
 },"useData":true});
 
 this["Fliplet"]["Widget"]["Templates"]["templates.noNotifications"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div class=\"no-notifications\">\r\n  <h3>No notifications yet</h3>\r\n  <p>Check again later for updates</p>\r\n</div>";
+    var alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3=container.escapeExpression;
+
+  return "<div class=\"no-notifications\">\r\n  <h3>"
+    + alias3((helpers.T || (depth0 && depth0.T) || alias2).call(alias1,"widgets.notificationInbox.noNotifications.title",{"name":"T","hash":{},"data":data}))
+    + "</h3>\r\n  <p>"
+    + alias3((helpers.T || (depth0 && depth0.T) || alias2).call(alias1,"widgets.notificationInbox.noNotifications.instruction",{"name":"T","hash":{},"data":data}))
+    + "</p>\r\n</div>\r\n";
 },"useData":true});
 
 this["Fliplet"]["Widget"]["Templates"]["templates.notification"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
@@ -15,11 +23,19 @@ this["Fliplet"]["Widget"]["Templates"]["templates.notification"] = Handlebars.te
 },"useData":true});
 
 this["Fliplet"]["Widget"]["Templates"]["templates.notificationsError"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div class=\"notifications-error\">\r\n  <h3>There was an error loading notifications</h3>\r\n</div>";
+    return "<div class=\"notifications-error\">\r\n  <h3>"
+    + container.escapeExpression((helpers.T || (depth0 && depth0.T) || helpers.helperMissing).call(depth0 != null ? depth0 : (container.nullContext || {}),"widgets.notificationInbox.errors.loadingFailed",{"name":"T","hash":{},"data":data}))
+    + "</h3>\r\n</div>\r\n";
 },"useData":true});
 
 this["Fliplet"]["Widget"]["Templates"]["templates.toolbar"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<p><span class=\"unread-count\">{{TN count}}</span> unread <span class=\"toolbar-read-all\">(<a data-read-all href=\"#\">Mark all as read</a>)</span><i class=\"fa fa-lg fa-fw fa-cog pull-right\" data-settings></i><i class=\"fa fa-lg fa-fw fa-refresh pull-right\" data-refresh></i></p>\r\n";
+    var alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3=container.escapeExpression;
+
+  return "<p><span class=\"unread-count\">{{TN count}}</span> "
+    + alias3((helpers.T || (depth0 && depth0.T) || alias2).call(alias1,"widgets.notificationInbox.toolbar.unread",{"name":"T","hash":{},"data":data}))
+    + " <span class=\"toolbar-read-all\">(<a data-read-all href=\"#\">"
+    + alias3((helpers.T || (depth0 && depth0.T) || alias2).call(alias1,"widgets.notificationInbox.toolbar.actions.readAll",{"name":"T","hash":{},"data":data}))
+    + "</a>)</span><i class=\"fa fa-lg fa-fw fa-cog pull-right\" data-settings></i><i class=\"fa fa-lg fa-fw fa-refresh pull-right\" data-refresh></i></p>\r\n";
 },"useData":true});
 
 this["Fliplet"]["Widget"]["Templates"]["templates.toolbar.empty"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
